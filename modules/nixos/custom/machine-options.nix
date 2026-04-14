ed89c70da4aed7f59f9b87c175e8edda6faeedbc
Desant <anturated@gmail.com>
2026-04-14 10:55:44 +0200

    feat: add option descriptions

--- modules/nixos/custom/machine-options.nix
+++ modules/nixos/custom/machine-options.nix
@@ -5,16 +5,19 @@
     isLaptop = lib.mkOption {
       type = lib.types.bool;
       default = false;
+      description = "Change to true if the device is a laptop. Used for brightness and other fixes";
     };
 
     boot = {
       quick = lib.mkOption {
         type = lib.types.bool;
         default = true;
+        description = "Speeds up boot by compressing initrd, early loading modules, etc.";
       };
       plymouth = lib.mkOption {
         type = lib.types.bool;
         default = false;
+        description = "Enable boot animation";
       };
     };
 
@@ -23,10 +26,12 @@
         enable = lib.mkOption {
           type = lib.types.bool;
           default = false;
+          description = "Enable AMD GPU drivers & optimizations";
         };
         busId = lib.mkOption {
           type = lib.types.string;
           default = "";
+          description = "GPU bus id used for pinning gpu to a set path and offloading";
         };
       };
 
@@ -34,20 +39,24 @@
         enable = lib.mkOption {
           type = lib.types.bool;
           default = false;
+          description = "Enable Nvidia GPU drivers & optimizations";
         };
         busId = lib.mkOption {
           type = lib.types.string;
           default = "";
+          description = "GPU bus id used for pinning gpu to a set path and offloading";
         };
 
         prime = {
           enable = lib.mkOption {
             type = lib.types.bool;
             default = false;
+            description = "Enable Optimus PRIME";
           };
           offload = lib.mkOption {
             type = lib.types.bool;
             default = false;
+            description = "Enable PRIME Offload";
           };
         };
       };
@@ -57,10 +66,12 @@
       timezone = lib.mkOption {
         type = lib.types.str;
         default = "";
+        description = "Shortcut for system.timezone nixos option";
       };
       locale = lib.mkOption {
         type = lib.types.str;
         default = "";
+        description = "Shortcut for system.locale nixos option";
       };
     };
   };
