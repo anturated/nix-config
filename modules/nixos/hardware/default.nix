9de3c15d941c8cf39a467142518781ba3010fd29
Desant <anturated@gmail.com>
2026-05-03 17:58:46 +0200

    refactor: bluetooth & firmware

---
+++ modules/nixos/hardware/default.nix
@@ -0,0 +1,11 @@
+{ ... }:
+
+{
+  imports = [
+    ./cpu
+    ./gpu
+    ./power
+    ./bluetooth.nix
+    ./firmware.nix
+  ];
+}
--- modules/nixos/hardware/firmware.nix
+++ modules/nixos/hardware/firmware.nix
@@ -1,5 +1,12 @@
-{ ... }:
+{ config, ... }:
 
 {
-  hardware.enableAllFirmware = true;
+  # enable non-free firmware
+  hardware.enableRedistributableFirmware = true;
+
+  # firmware updater for machine hardware
+  services.fwupd = {
+    enable = true;
+    daemonSettings.EspLocation = config.boot.loader.efi.efiSysMountPoint;
+  };
 }
