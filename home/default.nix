86feb79e55530b42c5ee1a10edd81fbcef898440
Desant <anturated@gmail.com>
2026-05-03 18:22:40 +0200

    refactor: move home

---
+++ home/default.nix
@@ -0,0 +1,27 @@
+{ user, ... }:
+{
+  home.username = "${user}";
+  home.homeDirectory = "/home/${user}";
+
+  imports = [
+    ./compositors/hyprland.nix
+
+    ./custom/monitors-options.nix
+
+    ./ricing/fastfetch.nix
+    ./ricing/starship.nix
+    ./ricing/kitty.nix
+    ./ricing/fish.nix
+    ./ricing/cava.nix
+    ./ricing/mangohud.nix
+    ./ricing/matugen.nix
+    ./ricing/rofi.nix
+    ./ricing/gtk.nix
+    ./ricing/qt.nix
+    ./ricing/vesktop.nix
+  ];
+
+  programs.home-manager.enable = true;
+
+  home.stateVersion = "25.05";
+}
