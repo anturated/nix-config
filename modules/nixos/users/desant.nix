ecd54b651b946888383df22f787325934d8f8448
Desant <anturated@gmail.com>
2026-05-04 18:07:48 +0200

    fix: wrong name

--- modules/nixos/users/desant.nix
+++ modules/nixos/users/desant.nix
@@ -8,7 +8,7 @@ let
 in
 {
   config = mkIf (elem "desant" (builtins.attrNames config.ceirios.system.users)) {
-    users.users.isabel = {
+    users.users.desant = {
       hashedPassword = "$y$j9T$gVFMTg9bPiCeQV1QmYCW80$Sow4EyH1UCKBRYra94EY1d2DFWKxE/0ZzADroGJzg/9";
     };
   };
