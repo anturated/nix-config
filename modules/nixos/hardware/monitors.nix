c63d801a0c7bd1de321b776bca5cc86d099e5cc2
Desant <anturated@gmail.com>
2026-05-04 18:51:58 +0200

    feat: add main monitor

--- modules/nixos/hardware/monitors.nix
+++ modules/nixos/hardware/monitors.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, config,... }:
 
 let
   inherit (lib) mkOption types;
@@ -11,53 +11,62 @@ let
     ;
 in
 {
-  options.ceirios.hardware.monitors = mkOption {
-    type = attrsOf (
-      submodule (
-        { name, ... }:
-        {
-          options = {
-            name = mkOption {
-              type = str;
-              default = name;
-              description = "Monitor name";
-              example = "DP-1";
-            };
+  # default = builtins.elemAt (builtins.attrNames config.ceirios.system.users) 0;
+  options.ceirios.hardware = {
+    mainMonitor = {
+      type = str;
+      default = builtins.elemAt (builtins.attrNames config.ceirios.hardware.monitors) 0;
+      description = "Main monitor's name.";
+    };
 
-            width = mkOption {
-              type = int;
-              default = 1920;
-              description = "Width of the monitor in pixels";
-              example = 1920;
-            };
+    monitors = mkOption {
+      type = attrsOf (
+        submodule (
+          { name, ... }:
+          {
+            options = {
+              name = mkOption {
+                type = str;
+                default = name;
+                description = "Monitor name";
+                example = "DP-1";
+              };
 
-            height = mkOption {
-              type = int;
-              default = 1080;
-              description = "Height of the monitor in pixels";
-              example = 1080;
-            };
+              width = mkOption {
+                type = int;
+                default = 1920;
+                description = "Width of the monitor in pixels";
+                example = 1920;
+              };
 
-            refresh-rate = mkOption {
-              type = int;
-              default = 60;
-              description = "Refresh rate of the monitor in hz";
-              example = 144;
-            };
+              height = mkOption {
+                type = int;
+                default = 1080;
+                description = "Height of the monitor in pixels";
+                example = 1080;
+              };
 
-            scale = mkOption {
-              type = float;
-              default = 1.0;
-              description = "Refresh rate of the monitor in hz";
-              example = 1.5;
-            };
-          };
-        }
-      )
-    );
+              refresh-rate = mkOption {
+                type = int;
+                default = 60;
+                description = "Refresh rate of the monitor in hz";
+                example = 144;
+              };
 
-    description = ''
-      Declare monitors in window managers
-    '';
+              scale = mkOption {
+                type = float;
+                default = 1.0;
+                description = "Refresh rate of the monitor in hz";
+                example = 1.5;
+              };
+            };
+          }
+        )
+      );
+
+      description = ''
+        Declare monitors in window managers
+      '';
+    };
   };
 }
