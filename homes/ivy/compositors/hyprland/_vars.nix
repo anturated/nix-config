e4bdf98e218a4dd8d213623fc8cf8935323b5058
Desant <anturated@gmail.com>
2026-05-05 05:04:44 +0200

    fix: monitor name

--- homes/ivy/compositors/hyprland/_vars.nix
+++ homes/ivy/compositors/hyprland/_vars.nix
@@ -1,8 +1,9 @@
+# TODO: there's probably fixes for all of this but im stupid
 {
   # Monitors — see components/monitors.nix
   # https://wiki.hypr.land/Configuring/Monitors/
   # https://wiki.hypr.land/Configuring/Keywords/#setting-the-configuration
-  mon1 = "eDP-1";
+  mon1 = "eDP-2";
   mon2 = "DP-1";
 
   # Apps
