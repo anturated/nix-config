b1194af09e6457468f77f6264b4f1628c85652a2
Desant <anturated@gmail.com>
2026-05-07 16:04:18 +0200

    feat: remove gpg agent

--- modules/home-manager/software/gpg-agent.nix
+++ modules/home-manager/software/gpg-agent.nix
@@ -1,18 +1,22 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
-  # client
-  ceirios.packages = { inherit (pkgs) gnupg; };
+  # we use ssh to sign stuff
+  # but since i made this i'll leave it here in case i might need it
+  config = lib.mkIf false {
+    # client
+    ceirios.packages = { inherit (pkgs) gnupg; };
 
-  # signing daemon
-  # https://search.nixos.org/options?channel=25.11&query=security.pam.services.%3Cname%3E.gnupg#show=option%253Asecurity.pam.services.%253Cname%253E.gnupg.enable
-  services.gpg-agent = {
-    enable = true;
+    # signing daemon
+    # https://search.nixos.org/options?channel=25.11&query=security.pam.services.%3Cname%3E.gnupg#show=option%253Asecurity.pam.services.%253Cname%253E.gnupg.enable
+    services.gpg-agent = {
+      enable = true;
 
-    # allow pam auto-unlock
-    extraConfig = "allow-preset-passphrase";
+      # allow pam auto-unlock
+      extraConfig = "allow-preset-passphrase";
+    };
+
+    # keygrip for pam auto-unlock
+    home.file.".pam-gnupg".text = "B7C1B3D12B33324577B6347325FCD30A4BF2C916\n";
   };
-
-  # keygrip for pam auto-unlock
-  home.file.".pam-gnupg".text = "B7C1B3D12B33324577B6347325FCD30A4BF2C916\n";
 }
