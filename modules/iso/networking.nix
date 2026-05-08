35c16a44119f89ca37f8716f3c82db8f18cf10e5
Desant <anturated@gmail.com>
2026-05-08 08:58:29 +0200

    fix: iso network

--- modules/iso/networking.nix
+++ modules/iso/networking.nix
@@ -14,7 +14,21 @@ in
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMOJoauZQLAdUyxVmB+oxNQK+LSQ1Y3/L///GjC+oQlG"
   ];
 
-  # enable global DHCP, solves network issues
-  # ik it's deprecated but it's good enough for an iso
-  useDHCP = true;
+  # NOTE: for some reason dhcp is very hard to set up on my vps
+  # we gotta go with networkd + cloud-init
+  # if we want the vps to see the light of day ever
+
+  # ask the host nicely for dhcp stuff
+  services.cloud-init.enable = true;
+
+  networking = {
+    # use systemd-networkd
+    useNetworkd = true;
+
+    # doesn't seem to work with cloud-init
+    networkmanager.enable = mkForce false;
+
+    # just to be sure
+    useDHCP = mkForce false;
+  };
 }
