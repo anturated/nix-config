cd6df58aaba8d1a6a1af1f7e1ff73b1e4fa511c2
Desant <anturated@gmail.com>
2026-05-09 16:27:53 +0200

    fix: remove my key from every user actually

--- homes/ivy/system/ssh.nix
+++ homes/ivy/system/ssh.nix
@@ -37,11 +37,4 @@
       };
     };
   };
-
-  home.file.".ssh/id_ed25519.pub".text = ''
-    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMOJoauZQLAdUyxVmB+oxNQK+LSQ1Y3/L///GjC+oQlG
-  '';
-
-  sops.secrets = {
-  };
 }
