5c953e42cdf789a2a4ea7d84590b43e2cff1d53d
Desant <anturated@gmail.com>
2026-05-11 19:33:08 +0200

    chore: move mkHosts closer to inputs

--- modules/flake/default.nix
+++ modules/flake/default.nix
@@ -4,14 +4,14 @@ let
   inherit (inputs) nixpkgs self;
   inherit (nixpkgs) lib;
 
+  mkHosts = lib.mapAttrs self.lib.mkHost;
+
   # auto-discover from machines/
   machineNames = builtins.attrNames (
     lib.filterAttrs (_: type: type == "directory") (builtins.readDir (self + "/machines"))
   );
 
   discovered = lib.genAttrs machineNames (_: { });
-
-  mkHosts = lib.mapAttrs self.lib.mkHost;
 in
 {
   lib = import ./lib { inherit lib inputs; };
