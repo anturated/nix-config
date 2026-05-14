c40017c2ffdb93c699462303bcb1cf15c4c7c2ac
Desant <anturated@gmail.com>
2026-05-14 13:00:41 +0200

    feat: ditch cloud-init

--- modules/iso/networking.nix
+++ modules/iso/networking.nix
@@ -13,17 +13,4 @@ in
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMOJoauZQLAdUyxVmB+oxNQK+LSQ1Y3/L///GjC+oQlG"
   ];
-
-  # autosetup network devices
-  # we GIVE the installer INTERNET.
-  # is this dirty? yes. but the iso has internet
-  services.cloud-init.enable = true;
-
-  # only usable with networkd
-  networking = {
-    useNetworkd = mkForce true;
-
-    useDHCP = mkForce false;
-    networkmanager.enable = mkForce false;
-  };
 }
